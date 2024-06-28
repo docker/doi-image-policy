a788a6c9e610cb8e8a5816434638a84a3ea87288
Jonny Stoten <jonny.stoten@docker.com>
2024-06-28 12:18:35 +0100

    Verify valid provenance and SBOM attestations
    These checks are pretty bare bones at the moment as we don't have much
    in these attestations that is consistent across all DOI that we can
    check for.
    
    There is a lot of duplication at the moment which I hope can be reduced,
    but I'm operating at the edge of my rego knowledge here!

--- test.sh
+++ test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-./image-signer-verifier.sh test -d policy/vsa "$@"
+
+fails=0
+
+./image-signer-verifier.sh test -d policy/full "$@" || ((fails++))
+./image-signer-verifier.sh test -d policy/vsa "$@" || ((fails++))
+
+if [ $fails -gt 0 ]; then
+    echo "Failed $fails tests"
+    exit 1
+fi
+echo "All tests passed"
