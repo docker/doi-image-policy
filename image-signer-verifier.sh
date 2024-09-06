f26b93c16f84c695b6651db2560bc6c4eea03b09
Jonny Stoten <jonny.stoten@docker.com>
2024-09-06 11:53:38 +0100

    Update to image-signer-verifier 0.5.20

--- image-signer-verifier.sh
+++ image-signer-verifier.sh
@@ -1,4 +1,4 @@
-isv_image="docker/image-signer-verifier:0.5.17@sha256:bde1b7cdd36933b620353aa83f4115cc13e1d6230902c7a892b1580520607b55"
+isv_image="docker/image-signer-verifier:0.5.20@sha256:c3017e07df0b5c0f7f50c0f73fef866ce673a780c59c201f1b564f83b5d5fb93"
 #isv_image="isv:latest"
 
 mkdir -p $HOME/.local/tmp/sigstore
